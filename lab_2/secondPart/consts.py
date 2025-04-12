f2eccdaa8b560b48d5f0112dd79e16d745b894f6
mappy-mac <mappydesuu@gmail.com>
2025-04-12 13:40:17 +0400

    added block run test

--- lab_2/secondPart/consts.py
+++ lab_2/secondPart/consts.py
@@ -1,1 +1,3 @@
-CPP_sequence_txt = "../firstPart/CPP_sequence.txt"
+cpp_sequence_txt = "../firstPart/CPP_sequence.txt"
+
+PI = [0.2148, 0.3672, 0.2305, 0.1875]
--- lab_2/secondPart/tests.py
+++ lab_2/secondPart/tests.py
@@ -1,27 +1,8 @@
 import math
 
+from scipy.special import gammainc
+
 from consts import *
-
-
-def frequency_bit_test(sequence: str) -> float:
-
-    n = len(sequence)
-
-    if n == 0:
-        raise ValueError("Sequence is empty")
-
-    s = sum([1 if bit == "1" else -1 for bit in sequence])
-
-    p_value = math.erfc((abs(s) / math.sqrt(n)) / math.sqrt(2))
-
-    return p_value
-
-
-def main():
-
-    CPP_sequence = read_file(CPP_sequence_txt)
-
-    print(frequency_bit_test(CPP_sequence))
 
 
 def read_file(filename: str) -> str:
@@ -48,6 +29,101 @@ def write_file(filename: str, text: str) -> None:
         print(f"Error writing file: {e}")
 
 
+def frequency_bit_test(sequence: str) -> float:
+
+    n = len(sequence)
+
+    if n == 0:
+        raise ValueError("Sequence is empty")
+
+    s = sum([1 if bit == "1" else -1 for bit in sequence])
+
+    p_value = math.erfc((abs(s) / math.sqrt(n)) / math.sqrt(2))
+
+    return p_value
+
+
+def runs_test(sequence: str) -> float:
+
+    p_value = 0
+
+    n = len(sequence)
+
+    p = sequence.count('1') / n
+
+    if abs(p - 0.5) >= 2 / math.sqrt(n):
+        return p_value
+
+    v_n = 0
+
+    for i in range(n):
+
+        if i == 0 or sequence[i] != sequence[i - 1]:
+            v_n += 1
+
+    numerator = abs(v_n - 2 * n * p * (1 - p))
+    denominator = 2 * math.sqrt(2 * n) * p * (1 - p)
+
+    return math.erfc(numerator / denominator)
+
+
+def block_run_test(sequence: str) -> float:
+
+    n = len(sequence)
+
+    if n < 128:
+        raise ValueError("Minimum 128 bits")
+
+    N = n // 8
+
+    v = [0, 0, 0, 0]
+
+    for i in range(N):
+
+        block = sequence[i * 8 : (i + 1) * 8]
+
+        max_run = 0
+        current_run = 0
+
+        for bit in block:
+
+            if bit == '1':
+                current_run += 1
+
+                if current_run > max_run:
+                    max_run = current_run
+
+            else:
+                current_run = 0
+
+        if max_run <= 1:
+            v[0] += 1
+
+        elif max_run == 2:
+            v[1] += 1
+
+        elif max_run == 3:
+            v[2] += 1
+
+        else:
+            v[3] += 1
+
+    x_2 = 0.0
+
+    for i in range(len(v)):
+        x_2 += (v[i] - 16 * PI[i]) ** 2 / (16 * PI[i])
+
+    p_value = gammainc(3 / 2, x_2 / 2)
+
+    return p_value
+
+
+def main():
+
+    cpp_sequence = read_file(cpp_sequence_txt)
+    print("Последовательность:", cpp_sequence)
+    print(block_run_test(cpp_sequence))
+
+
 if __name__ == "__main__":
     main()
-
