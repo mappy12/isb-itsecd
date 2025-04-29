3c66d8c178676d8fbfcb4f8d35889886b6e482c0
mappy12 <mappydesuu@gmail.com>
2025-04-29 21:50:57 +0400

    added generate assymetric key method

--- lab_3/keygen.py
+++ lab_3/keygen.py
@@ -1,5 +1,9 @@
 import os
 
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+
 class SymmetricEncryption:
 
     @staticmethod
@@ -12,3 +16,17 @@ class SymmetricEncryption:
 
         return key
 
+
+class AssymetricEncryption:
+
+    @staticmethod
+    def generate_assym_key() -> tuple:
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+
+        public_key = private_key.public_key()
+
+        return private_key, public_key
