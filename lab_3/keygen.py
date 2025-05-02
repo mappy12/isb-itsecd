c61bbb2a014979dacfc843a96e5b40158ff081b3
mappy12 <mappydesuu@gmail.com>
2025-05-02 13:05:00 +0400

    updated keygen methods

--- lab_3/keygen.py
+++ lab_3/keygen.py
@@ -8,13 +8,29 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 class SymmetricKey:
 
     @staticmethod
+    def get_key_length() -> int:
+
+        while True:
+
+            try:
+
+                key_length = int(input("Введите длину симметричного ключа: "))
+
+                if key_length < 32 or key_length > 448 or key_length % 8 != 0:
+                    print("Blowfish key length must be between 32 and 448 bits, in 8-bit steps.")
+
+                else:
+                    return key_length
+
+            except ValueError:
+
+                print("Ошибка: введите целое число!")
+
+
+    @staticmethod
     def generate_sym_key(key_length: int) -> bytes:
 
-        if key_length < 32 or key_length > 448 or key_length % 8 != 0:
-            raise ValueError("Blowfish key length must be between 32 and 448 bits, in 8-bit steps.")
-
         key = os.urandom(key_length // 8)
-
         return key
 
 
