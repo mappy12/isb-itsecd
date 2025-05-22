cea73d2ad893c033b41a306a7c4e37183e73215f
mappy12 <mappydesuu@gmail.com>
2025-05-22 21:13:01 +0400

    added time measurement for searching for a card number with different numbers of processes

---
+++ lab_4/collision_timing.py
@@ -0,0 +1,32 @@
+import time
+
+from pandas.core.indexing import check_dict_or_set_indexers
+
+from card_finder import *
+
+
+def find_card_number_time(bin_code: str, process_count: int) -> float:
+    card_nums = gen_card_nums(bin_code)
+
+    start = time.time()
+    with multiprocessing.Pool(process_count) as pool:
+
+        for result in pool.imap_unordered(check_hash, card_nums, chunksize=500):
+            if result:
+                break
+
+    end = time.time()
+
+    return end - start
+
+
+def collect_times(bin_code: str) -> tuple[list[int], list[float]]:
+    max_cpu = int(get_cpu_count() * 1.5)
+    process_counts = list(range(1, max_cpu + 1))
+    timings = []
+
+    for count in process_counts:
+        duration = find_card_number_time(bin_code, count)
+        timings.append(duration)
+
+    return process_counts, timings
