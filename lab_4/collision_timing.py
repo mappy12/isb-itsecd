34ffd8b6e9adeb4f5358354324a54e6d83049a1a
mappy12 <mappydesuu@gmail.com>
2025-05-24 14:57:19 +0400

    added the ability to select a custom operating mode

--- lab_4/collision_timing.py
+++ lab_4/collision_timing.py
@@ -5,28 +5,30 @@ import matplotlib.pyplot as plt
 from card_finder import *
 
 
-def find_card_number_time(bin_code: str, process_count: int) -> float:
-    card_nums = gen_card_nums(bin_code)
-
+def find_card_number_time(process_count: int) -> float:
     start = time.time()
     with multiprocessing.Pool(process_count) as pool:
+        for bin_code in BINS:
+            card_nums = gen_card_nums(bin_code)
 
-        for result in pool.imap_unordered(check_hash, card_nums, chunksize=500):
-            if result:
-                break
+            for result in pool.imap_unordered(check_hash, card_nums, chunksize=500):
+                if result:
+                    pool.terminate()
+                    pool.join()
+                    end = time.time()
+                    return end - start
 
     end = time.time()
-
     return end - start
 
 
-def collect_times(bin_code: str) -> tuple[list[int], list[float]]:
+def collect_times() -> tuple[list[int], list[float]]:
     max_cpu = int(get_cpu_count() * 1.5)
     process_counts = list(range(1, max_cpu + 1))
     timings = []
 
     for count in process_counts:
-        duration = find_card_number_time(bin_code, count)
+        duration = find_card_number_time(count)
         timings.append(duration)
 
     return process_counts, timings
--- lab_4/main.py
+++ lab_4/main.py
@@ -1,10 +1,76 @@
+import argparse
+
 from card_finder import *
+from collision_timing import *
 from luhn import *
 
-def main():
-    serialization(find_card_number())
 
-    is_valid(find_card_number())
+def mode_1():
+    print("MODE 1: Подбор номера карты по ее хешу с использованием оптимального числа процессов\n")
+
+    cpu_count = get_cpu_count()
+
+    print(f"Используемое число процессов: {cpu_count}")
+    card_nums = find_card_number()
+
+    if card_nums:
+        print(f"Найденный номер карты: {card_nums}\n")
+        serialization(card_nums)
+        print("Результат сериализован в result.json")
+
+    else:
+        print("Номер карты не найден")
+
+
+def mode_2():
+    print("MODE 2: Проверка карты алгоритмом Луна (корректность номера)\n")
+
+    card_nums = find_card_number()
+    print(f"Номер карты: {card_nums}\n")
+
+    print("Проверка:\n")
+    is_valid(card_nums)
+
+
+def mode_3():
+    print("MODE 3: Замер времени поиска коллизии хеша при различном числе процессов\n")
+
+    print("Обработка ->...\n")
+
+    process_counts, timings = collect_times()
+    print("Число процессов:")
+    print(process_counts)
+    print("Время:")
+    print(timings)
+    print("\n\n")
+
+    get_timing_graph(process_counts, timings)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "mode",
+        type=int,
+        choices=[1,2,3],
+        help="Режимы работы: \n"
+             "1 - Подбор номера карты по хэшу\n"
+             "2 - Проверка корректности номера алгоритмом Луна\n"
+             "3 - Замер времени подбора при разном числе процессов"
+    )
+
+    args = parser.parse_args()
+
+    match args.mode:
+
+        case 1:
+            mode_1()
+        case 2:
+            mode_2()
+        case 3:
+            mode_3()
+
 
 if __name__ == "__main__":
     main()
